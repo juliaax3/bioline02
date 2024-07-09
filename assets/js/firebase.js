f19a4e6d468a62e113e54f72b9bd8c0f48a26112
gabiiizinha <rodriguesgabrielle2501@gmail.com>
2024-07-09 16:49:15 -0300

    Criação da função de envio de dados para o banco de dados

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import{getDatabase} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJgqb5d9b9tg0SHepMydGiPXEVZBAOuS0",
@@ -8,10 +9,12 @@ const firebaseConfig = {
   storageBucket: "jujuba-cf4e9.appspot.com",
   messagingSenderId: "807516463621",
   appId: "1:807516463621:web:d68dc093b8661d4ba3f26c",
-  measurementId: "G-4M812KH9YJ"
+  measurementId: "G-4M812KH9YJ",
+  databaseURL: "https://jujuba-cf4e9-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-  export {app, auth}
+  export {app, auth, database}
